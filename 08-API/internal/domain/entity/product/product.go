c331417b5ddb6440d9e14d140197652ad9f8c7a8
KelpGF <kelpgf@gmail.com>
2024-06-25 19:21:32 -0300

    feat(08): add product entity

---
+++ 08-API/internal/domain/entity/product/product.go
@@ -0,0 +1,59 @@
+package product
+
+import (
+	"errors"
+	"time"
+
+	"github.com/KelpGF/Go-Expert/08-APIs/pkg/entity"
+)
+
+var (
+	ErrIdIsRequired    = errors.New("id is required")
+	ErrInvalidID       = errors.New("id is invalid")
+	ErrNameIsRequired  = errors.New("name is required")
+	ErrPriceIsRequired = errors.New("price is required")
+	ErrInvalidPrice    = errors.New("price must be greater than 0")
+)
+
+type Product struct {
+	ID        entity.ID `json:"id"`
+	Name      string    `json:"name"`
+	Price     float64   `json:"price"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func NewProduct(id entity.ID, name string, price float64) (*Product, error) {
+	product := &Product{
+		ID:        id,
+		Name:      name,
+		Price:     price,
+		CreatedAt: time.Now(),
+	}
+
+	if err := product.Validate(); err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (product *Product) Validate() error {
+	if product.ID.String() == "" {
+		return ErrIdIsRequired
+	}
+
+	if _, err := entity.ParseID(product.ID.String()); err != nil {
+		return ErrInvalidID
+	}
+
+	if product.Name == "" {
+		return ErrNameIsRequired
+	}
+	if product.Price == 0 {
+		return ErrPriceIsRequired
+	}
+	if product.Price < 0 {
+		return ErrInvalidPrice
+	}
+	return nil
+}
---
+++ 08-API/internal/domain/entity/product/product_test.go
@@ -0,0 +1,43 @@
+package product
+
+import (
+	"testing"
+
+	"github.com/KelpGF/Go-Expert/08-APIs/pkg/entity"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewProduct(t *testing.T) {
+	product, err := NewProduct(entity.NewID(), "product", 10.0)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, product)
+	assert.NotEmpty(t, product.ID)
+	assert.Equal(t, "product", product.Name)
+	assert.Equal(t, 10.0, product.Price)
+	assert.NotZero(t, product.CreatedAt)
+}
+
+func TestNewProductWhenNameIsEmpty(t *testing.T) {
+	product, err := NewProduct(entity.NewID(), "", 10.0)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, product)
+	assert.Equal(t, ErrNameIsRequired, err)
+}
+
+func TestNewProductWhenPriceIsZero(t *testing.T) {
+	product, err := NewProduct(entity.NewID(), "product", 0)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, product)
+	assert.Equal(t, ErrPriceIsRequired, err)
+}
+
+func TestNewProductWhenPriceIsNegative(t *testing.T) {
+	product, err := NewProduct(entity.NewID(), "product", -10.0)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, product)
+	assert.Equal(t, ErrInvalidPrice, err)
+}
